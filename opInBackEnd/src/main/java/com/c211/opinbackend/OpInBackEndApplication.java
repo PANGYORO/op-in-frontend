368480e597a201faf607772ae5e52396ec0992eb
ManduTheCat <krocd@naver.com>
2023-01-20 21:33:14 +0900

    init backend PJ

---
+++ opInBackEnd/src/main/java/com/c211/opinbackend/OpInBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.c211.opinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpInBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpInBackEndApplication.class, args);
+	}
+
+}
