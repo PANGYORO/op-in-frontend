5d3d8a877513fca7f3fa553703d2041367ab226c
chosunguk <saeloun8797@gmail.com>
2023-01-30 15:17:33 +0900

    S08P12C211-371 logo 생성

--- op-in-front/src/pages/user/SignIn.jsx
+++ op-in-front/src/pages/user/SignIn.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 
-
+import Logo from "../../components/Logo";
 
 
 
@@ -185,12 +185,12 @@ function SignUpForm() {
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8 bg-gray-50 p-5">
-        {/* <div>
+        <div>
           <Logo className="mx-auto h-20 w-auto" />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
             Sign <span className="text-blue-600">up</span> to your account
           </h2>
-        </div> */}
+        </div>
         <div className="shadow-md p-5 mb-4 bg-white">
           <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
             <Nickname
