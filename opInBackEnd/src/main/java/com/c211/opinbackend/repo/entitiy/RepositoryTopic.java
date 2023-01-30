6834a4006ab324dc58f9b9d8526439f6bbc13d12
ManduTheCat <krocd@naver.com>
2023-01-31 02:17:14 +0900

    S08P12C211-369 fix: 래이지 로딩 케스케이딩 추가 래포지토리 토픽 엔티티에

--- opInBackEnd/src/main/java/com/c211/opinbackend/repo/entitiy/RepositoryTopic.java
+++ opInBackEnd/src/main/java/com/c211/opinbackend/repo/entitiy/RepositoryTopic.java
@@ -9,8 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.c211.opinbackend.auth.entity.Topic;
-
 import lombok.Getter;
 
 @Entity
@@ -21,11 +19,11 @@ public class RepositoryTopic {
 	@Column(name = "REPOSITORY_TOPIC_ID")
 	private Long id;
 
-	@ManyToOne( fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "TOPIC_ID")
 	private Topic topic;
 
-	@ManyToOne( fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "REPOSITORY_ID")
 	private Repository repository;
 
