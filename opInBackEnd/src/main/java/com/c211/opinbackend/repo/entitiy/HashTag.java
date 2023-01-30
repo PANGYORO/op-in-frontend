5554de3e16c85f8e8ab7c6a6be860f3d499d0e52
ManduTheCat <krocd@naver.com>
2023-01-30 17:30:54 +0900

    S08P12C211-369 add: HashTag엔티티 작성

---
+++ opInBackEnd/src/main/java/com/c211/opinbackend/repo/entitiy/HashTag.java
@@ -0,0 +1,28 @@
+package com.c211.opinbackend.repo.entitiy;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class HashTag {
+	@Id
+	@GeneratedValue
+	@Column(name = "HASH_TAG")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "REPOSITORY_POST_ID")
+	private RepositoryPost repositoryPost;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "REPOSITORY_QNA_ID")
+	private RepositoryQnA repositoryQnA;
+
+
+}
