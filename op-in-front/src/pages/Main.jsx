89367262a9f14b0edcc215a4c96ee6219ac5e12f
kcy0521 <kyle.kcy@gmail.com>
2023-01-31 15:16:10 +0900

    S08P12C211-373 header,sidebar 중간완성

--- op-in-front/src/pages/Main.jsx
+++ op-in-front/src/pages/Main.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+// import { Router,useLocation } from 'react-router-dom';
 import Header from "../components/Header";
 import Sidebar from "../components/SideBar";
 
+
 export default function Main() {
+  // const [subDomain] = useLocation();
   return (
     <div>
       <div>
         <Header />
-        <Sidebar />
+        <main className="relative h-screen overflow-hidden bg-gray-100 dark:bg-gray-800 rounded-2xl">
+          <Sidebar />
+          {/* <Router subDomain ? component TK>
+            
+          </Router> */}
+        </main>
       </div>
     </div>
   );
