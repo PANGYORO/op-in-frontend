a4b45924e61e523f06f0beec085c65abbf9f866f
Sungwook Cho <saeloun8797@gmail.com>
2023-02-01 20:53:53 +0900

    S08P12C211-371 recoil 환경설정

--- op-in-front/src/main.jsx
+++ op-in-front/src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import {RecoilRoot} from "recoil";
 
 import Main from "./pages/Main";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <RouterProvider router={pages} /> */}
-    <Main />
+    <RecoilRoot>
+      {/* <RouterProvider router={pages} /> */}
+      <Main />
+    </RecoilRoot>
   </React.StrictMode>
 );
