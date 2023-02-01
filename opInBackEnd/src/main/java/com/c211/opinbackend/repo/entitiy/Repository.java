67bc56412cd1603f21ea65340e26b9c829d2140f
ManduTheCat <krocd@naver.com>
2023-02-01 14:41:22 +0900

    S08P12C211-369 hotfix: 맴버 수정 및 양방향 create 필요합니다.

--- opInBackEnd/src/main/java/com/c211/opinbackend/repo/entitiy/Repository.java
+++ opInBackEnd/src/main/java/com/c211/opinbackend/repo/entitiy/Repository.java
@@ -1,15 +1,15 @@
 package com.c211.opinbackend.repo.entitiy;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.c211.opinbackend.auth.entity.Member;
 
 import lombok.Getter;
 
@@ -25,7 +25,8 @@ public class Repository {
 	private TitleContent titleContent;
 	private String githubAddress;
 
-	@OneToMany(mappedBy = "repository", fetch = FetchType.LAZY)
-	private List<CommitHistory> commitHistory = new ArrayList<>();
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "MEMBER_ID")
+	private Member member;
 
 }
