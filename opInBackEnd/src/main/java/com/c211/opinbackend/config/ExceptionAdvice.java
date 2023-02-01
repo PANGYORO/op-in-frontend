29fb69a79969d597283d1966a6dc776141a54799
ManduTheCat <krocd@naver.com>
2023-02-01 12:04:08 +0900

    S08P12C211-369 add : 404 커스텀

--- opInBackEnd/src/main/java/com/c211/opinbackend/config/ExceptionAdvice.java
+++ opInBackEnd/src/main/java/com/c211/opinbackend/config/ExceptionAdvice.java
@@ -1,9 +1,14 @@
 package com.c211.opinbackend.config;
 
+import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
+import com.c211.opinbackend.exceoption.api.ApiExceptionEnum;
+import com.c211.opinbackend.exceoption.api.ApiRuntimeException;
+import com.c211.opinbackend.exceoption.auth.AuthRuntimeException;
 import com.c211.opinbackend.exceoption.member.MemberExceptionEntity;
 import com.c211.opinbackend.exceoption.member.MemberRuntimeException;
 
@@ -19,10 +24,37 @@ public class ExceptionAdvice {
 	 * @return
 	 */
 	@ExceptionHandler({MemberRuntimeException.class})
-	private ResponseEntity<MemberExceptionEntity> apiExceptionHandler(
+	private ResponseEntity<MemberExceptionEntity> memberExceptionHandler(
 		final MemberRuntimeException runError) {
 		return new ResponseEntity<>(
 			new MemberExceptionEntity(runError.getErrorEnum().getHttpStatus(), runError.getMessage()),
 			runError.getErrorEnum().getHttpStatus());
 	}
+
+	/**
+	 * @param exception
+	 * 존재하지 않는 API에 대한 기본 메세지 처리입니다.
+	 */
+	@ExceptionHandler({NoHandlerFoundException.class})
+	private ResponseEntity<MemberExceptionEntity> apiExceptionHandler(
+		final Exception exception) {
+		ApiRuntimeException runError = new ApiRuntimeException(ApiExceptionEnum.API_NOT_EXIST_EXCEPTION);
+		return new ResponseEntity<>(
+			new MemberExceptionEntity(runError.getErrorEnum().getHttpStatus(), runError.getMessage()),
+			runError.getErrorEnum().getHttpStatus());
+	}
+
+	/**
+	 * 권한과 인증관련된애러 처리입니다.
+	 * @param runError
+	 * @return
+	 */
+	@ExceptionHandler({AuthenticationException.class})
+	private ResponseEntity<MemberExceptionEntity> authExceptionHandler(
+		final AuthRuntimeException runError) {
+		return new ResponseEntity<>(
+			new MemberExceptionEntity(runError.getErrorEnum().getHttpStatus(), runError.getMessage()),
+			runError.getErrorEnum().getHttpStatus());
+	}
+
 }
