4988bcece1b49859b4b1b5f27d6dcc6ba1c376c0
kcy0521 <kyle.kcy@gmail.com>
2023-02-02 17:57:28 +0900

    S08P12C211-373 repo main css

--- op-in-front/src/components/Repo.jsx
+++ op-in-front/src/components/Repo.jsx
@@ -10,38 +10,49 @@ import axios from "axios";
 export default function Repo() {
     return (
         <>
-        
-        <div className="flex flex-col grid grid-row-3 bg-white dark:bg-gray-800 w-full shadow-lg rounded-xl p-4">
-            <div>
-                <span class='text-lg font-bold'>참여레포</span>
-            </div>
-            <div className="grid grid-cols-1 place-contnet-start">
-                <p class="text-gray-600 dark:text-white my-5 pl-2">
-                    <span class="text-lg font-bold text-indigo-500 ">
-                        “
-                    </span>
-                    내용칸 입니다.
-                    내용칸 입니다.
-                    <span class="text-lg font-bold text-indigo-500">
-                        ”
-                    </span>
-                </p>
-            </div>
-            <div className='grid grid-col place-content-bottom'>
-                <div class="inline-flex items-center bg-white leading-none ${props.textColor} rounded-full p-2 shadow text-teal text-sm">
-                    <div class="flex flex-wrap items-center gap-4">
-                        <span class="px-4 py-1  text-base rounded-full text-red-600  bg-red-200 ">
-                            java
-                        </span>
-                        <span class="px-4 py-1  text-base rounded-full text-yellow-600  bg-yellow-200 ">
-                            jupiter notebook
-                        </span>
-                        <span class="px-4 py-1  text-base rounded-full text-green-600  bg-green-200 ">
-                            python
-                        </span>
+            <div className="w-full inset-x-0 bottom-0 bg-white dark:bg-gray-800 shadow-lg rounded-xl p-4 flex flex-col">
+                <div>
+                    <div className='truncate'>
+                        <span class='text-lg font-bold'>참여레포"이 말이 지워지는지 확인하고 싶은데 어떻게 생각하세요dydydydydydy"</span>
+                    </div>
+                    <div className='w-full h-24 text-clip overflow-hidden'>
+                        <p class="text-gray-600 dark:text-white my-5 pl-2">
+                            내용칸 입니다.
+                            내용칸 입니다.
+                            이 말이 지워지는지 확인하고 싶은데 어떻게 생각하세요 
+                            이 말이 지워지는지 확인하고 싶은데 어떻게 생각하세요 
+                            이 말이 지워지는지 확인하고 싶은데 어떻게 생각하세요 
+                            이 말이 지워지는지 확인하고 싶은데 어떻게 생각하세요 
+                            이 말이 지워지는지 확인하고 싶은데 어떻게 생각하세요 
+                            이 말이 지워지는지 확인하고 싶은데 어떻게 생각하세요 
+                            이 말이 지워지는지 확인하고 싶은데 어떻게 생각하세요 
+                            이 말이 지워지는지 확인하고 싶은데 어떻게 생각하세요 
+                        </p>
                     </div>
                 </div>
-            </div>
+                {/* 여기 언어 공간 */}
+                <div>
+                    <div class="inline-flex items-center bg-white leading-none ${props.textColor} rounded-lg p-2 shadow text-teal text-sm">
+                        <div class="flex flex-wrap items-center gap-4">
+                            <span class="px-4 py-1  text-base rounded-full text-red-600  bg-red-200 ">
+                                java
+                            </span>
+                            <span class="px- py-1  text-base rounded-full text-yellow-600  bg-yellow-200 ">
+                                jupiter notebook
+                            </span>
+                            <span class="px-4 py-1  text-base rounded-full text-green-600  bg-green-200 ">
+                                python
+                            </span>
+                            <span class="px-4 py-1  text-base rounded-full text-green-600  bg-green-200 ">
+                                python
+                            </span>
+                            <span class="px-4 py-1  text-base rounded-full text-green-600  bg-green-200 ">
+                                python
+                            </span>
+                           
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </>
--- op-in-front/src/pages/Repository.jsx
+++ op-in-front/src/pages/Repository.jsx
@@ -27,20 +27,24 @@ export default function Repository({
       <div className="flex flex-col w-full pl-0 md:p-4">
         
 
-        <div className="flex items-top w-full">
+        <div className="flex items-top w-full h-screen pt-2 pb-24 pl-2 pr-2 overflow-auto md:pt-0 md:pr-0 md:pl-0"
+        >
           <div class="grid grid-cols-2  gap-4 w-full">
             <Repo />
-            <QnA />
-            <QnA />
+            <Repo />
+            <Repo />
+            <Repo />
+            <Repo />
+            <Repo />
             <Repo />
             <Repo />
             <TestRR />
        
           </div>
           
-          <div class="w-1/3">
+          {/* <div class="w-1/3">
             <Status />
-          </div>
+          </div> */}
 
         </div>
       </div>
