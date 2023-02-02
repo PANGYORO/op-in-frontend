16c296971483fc373ed33a0ddf916333eaea6eb9
kcy0521 <kyle.kcy@gmail.com>
2023-02-02 14:05:10 +0900

    S08P12C211-373 repo css작업

--- op-in-front/src/components/Repo.jsx
+++ op-in-front/src/components/Repo.jsx
@@ -1,38 +1,49 @@
 import React from "react";
+import axios from "axios";
 
+// const Reopitem = ({ repos }) => {
+//     const { img, name, contributor, star } = repos;
+//     return (
 
+//     )
+//   }
 export default function Repo() {
     return (
-            <div class="bg-white dark:bg-gray-800 w-full shadow-lg rounded-xl p-4">
-                <div class='grid grid-cols-4 gap-4'>
-                    <span class='col-start-1 col-end-3 text-lg font-bold'>참여레포</span>
-                </div>
-
+        <>
+        
+        <div className="flex flex-col grid grid-row-3 bg-white dark:bg-gray-800 w-full shadow-lg rounded-xl p-4">
+            <div>
+                <span class='text-lg font-bold'>참여레포</span>
+            </div>
+            <div className="grid grid-cols-1 place-contnet-start">
                 <p class="text-gray-600 dark:text-white my-5 pl-2">
-                    <span class="text-lg font-bold text-indigo-500">
+                    <span class="text-lg font-bold text-indigo-500 ">
                         “
                     </span>
+                    내용칸 입니다.
                     내용칸 입니다.
                     <span class="text-lg font-bold text-indigo-500">
                         ”
                     </span>
                 </p>
-                <div>
-                    <div class="inline-flex items-center bg-white leading-none ${props.textColor} rounded-full p-2 shadow text-teal text-sm">
-                        <div class="flex flex-wrap items-center gap-4">
-                            <span class="px-4 py-1  text-base rounded-full text-red-600  bg-red-200 ">
-                                java
-                            </span>
-                            <span class="px-4 py-1  text-base rounded-full text-yellow-600  bg-yellow-200 ">
-                                jupiter notebook
-                            </span>
-                            <span class="px-4 py-1  text-base rounded-full text-green-600  bg-green-200 ">
-                                python
-                            </span>
-                        </div>
+            </div>
+            <div className='grid grid-col place-content-bottom'>
+                <div class="inline-flex items-center bg-white leading-none ${props.textColor} rounded-full p-2 shadow text-teal text-sm">
+                    <div class="flex flex-wrap items-center gap-4">
+                        <span class="px-4 py-1  text-base rounded-full text-red-600  bg-red-200 ">
+                            java
+                        </span>
+                        <span class="px-4 py-1  text-base rounded-full text-yellow-600  bg-yellow-200 ">
+                            jupiter notebook
+                        </span>
+                        <span class="px-4 py-1  text-base rounded-full text-green-600  bg-green-200 ">
+                            python
+                        </span>
                     </div>
                 </div>
+            </div>
 
             </div>
+        </>
     )
 }
--- op-in-front/src/pages/Repository.jsx
+++ op-in-front/src/pages/Repository.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-// import Status from "../components/Status";
+import Status from "../components/Status";
 import Repo from "../components/Repo";
 import Sidebar from "../components/SideBar";
+import QnA from "../components/QnA"
+import TestRR from "../components/TestRR"
 
-export default function Repository() {
+
+
+
+
+
+export default function Repository({
+  // repos = [{
+  //   title: '우리레포는요...',
+  //   content: '내용이 어쩌구 저쩌고',
+  //   language: 'python',
+  //   contributor: [{ nickname: '짱짱맨' },{id:''}, {profileimg:''}],
+  //   star: 341110,
+  //   forknum : 321,
+  //   topic: 'react',
+  //   updatedate: '2021-01-02'
+  // }]
+
+}) {
   return (
     <>
       <Sidebar />
@@ -13,20 +32,18 @@ export default function Repository() {
 
         <div className="flex items-top w-full">
           <div class="grid grid-cols-2  gap-4 w-full">
-              <Repo />
-              <Repo />
-              <Repo />
-              <Repo />
-              <Repo />
-              <Repo />
-              <Repo />
-              <Repo />
-              <Repo />
+            <Repo />
+            <QnA />
+            <QnA />
+            <Repo />
+            <Repo />
+            <TestRR />
+       
           </div>
           
-          {/* <div class="w-1/3">
+          <div class="w-1/3">
             <Status />
-          </div> */}
+          </div>
 
         </div>
       </div>
