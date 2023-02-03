298d808b1d9139737d253615d512881013a386ce
djunnni <djunnni@gmail.com>
2023-02-03 16:02:27 +0900

    delete(Menus): Main에서 route 처리하기

--- op-in-front/src/pages/Main.jsx
+++ op-in-front/src/pages/Main.jsx
@@ -1,39 +1,30 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import SignIn from "./user/SignIn";
 import SignUp from "./user/SignUp";
 import UserFind from "./user/UserFind";
-import Menus from "./Menus";
 import Detail from "./user/Detail";
 import SelectTag from "./user/SelectTag";
 
-// import { useRecoilState } from "recoil";
-// import { userStored } from "../recoil/user/atoms";
-// import { useEffect } from "react";
-
-// import DashBoard from "./DashBoard";
-// import Repository from "./Repository";
-// import Education from "./Education";
-// import Events from "./Events";
 import Search from "./Search";
 import NotFound from "./NotFound";
+import Sidebar from "../components/SideBar";
+import DashBoard from "./DashBoard";
+import Repository from "./repository/Repository";
+import Education from "./Education";
+import Events from "./Events";
+
+function Template() {
+  return (
+    <div className="flex items-start justify-between mx-44">
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
+}
 
 export default function Main() {
-  // const [subDomain] = useLocation();
-  // const [user, setUser] = useRecoilState(userStored);
-  // useEffect(() => {
-  //   setUser([
-  //     {
-  //       id: 100,
-  //       name: "Keane Sykes",
-  //       address: "Ap #765-5550 A, Av.",
-  //       phone: "010-1152-3825",
-  //       memo: "luctus, ipsum leo elementum sem, vitae aliquam eros turpis non",
-  //     },
-  //   ]);
-  // }, []);
-
   return (
     <BrowserRouter>
       <div className="Main">
@@ -46,7 +37,12 @@ export default function Main() {
             <Route path="userfind" element={<UserFind />} />
             <Route path="detail" element={<Detail />} />
             <Route path="selecttag" element={<SelectTag />} />
-            <Route exact path="/*" element={<Menus />}></Route>
+            <Route exact path="/" element={<Template />}>
+              <Route exact index element={<DashBoard />} />
+              <Route path="repo" element={<Repository />} />
+              <Route path="edu" element={<Education />} />
+              <Route path="events" element={<Events />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
