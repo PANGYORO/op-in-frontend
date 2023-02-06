94fc716f39a2e0ecc9c302512857374de6da13a0
djunnni <djunnni@gmail.com>
2023-02-06 16:57:23 +0900

    fix(github): cookie 이름 수정

--- opInBackEnd/src/main/java/com/c211/opinbackend/auth/controller/OAuthController.java
+++ opInBackEnd/src/main/java/com/c211/opinbackend/auth/controller/OAuthController.java
@@ -10,29 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import com.c211.opinbackend.auth.constant.GitHub;
-import com.c211.opinbackend.auth.entity.Role;
 import com.c211.opinbackend.auth.jwt.JwtFilter;
-import com.c211.opinbackend.auth.model.MemberDto;
 import com.c211.opinbackend.auth.model.TokenDto;
-import com.c211.opinbackend.auth.model.request.OAuthAccessTokenRequest;
-import com.c211.opinbackend.auth.model.response.OAuthAccessTokenResponse;
 import com.c211.opinbackend.auth.service.OAuthService;
 import com.c211.opinbackend.auth.service.OAuthServiceImpl;
 import com.c211.opinbackend.exception.member.MemberRuntimeException;
-import com.c211.opinbackend.util.RandomString;
 
 @RestController
 @RequestMapping("/oauth")
@@ -47,27 +35,33 @@ public class OAuthController {
 	}
 
 	@GetMapping("/redirect/github")
-	public void redirectGithub(HttpServletResponse response, @RequestParam(value = "redirect_uri", required = false) String redirectUri) throws IOException {
+	public void redirectGithub(HttpServletResponse response,
+		@RequestParam(value = "redirect_uri", required = false) String redirectUri) throws IOException {
 		response.sendRedirect(oAuthService.getRedirectURL(redirectUri));
 	}
 
 	@GetMapping("/login/github")
-	public void getUserInfo(@RequestParam String code, @RequestParam(value = "redirect_uri", required = false) String redirectUri, HttpServletResponse response) {
+	public void getUserInfo(@RequestParam String code,
+		@RequestParam(value = "redirect_uri", required = false) String redirectUri, HttpServletResponse response) {
 		try {
 			TokenDto token = oAuthService.login(code, redirectUri);
 			HttpHeaders httpHeaders = new HttpHeaders();
 			httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + token.getAccessToken());
 
-			Cookie accessTokenCookie = new Cookie("access_token", token.getAccessToken());
-			Cookie refreshTokenCookie = new Cookie("refresh_token", token.getRefreshToken());
+			Cookie accessTokenCookie = new Cookie("accessToken", token.getAccessToken());
+			Cookie refreshTokenCookie = new Cookie("refreshToken", token.getRefreshToken());
+			Cookie typeCookie = new Cookie("type", token.getType());
+
 			accessTokenCookie.setPath("/");
 			refreshTokenCookie.setPath("/");
+			typeCookie.setPath("/");
 
 			response.addCookie(accessTokenCookie);
 			response.addCookie(refreshTokenCookie);
-
+			response.addCookie(typeCookie);
+			
 			response.sendRedirect(redirectUri);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new MemberRuntimeException(MEMBER_WRONG_EXCEPTION);
 		}
