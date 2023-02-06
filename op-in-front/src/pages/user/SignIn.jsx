8ea1594e3c1ddec64325f87041a14b14e1ab0bde
djunnni <djunnni@gmail.com>
2023-02-06 15:14:10 +0900

    fix(env): env domain 설정

--- op-in-front/src/pages/user/SignIn.jsx
+++ op-in-front/src/pages/user/SignIn.jsx
@@ -3,12 +3,10 @@ import { useForm } from "react-hook-form";
 import React from "react";
 
 import Logo from "../../components/Logo";
-import http from '../../api/http';
-import { useEffect } from 'react';
+import http from "../../api/http";
+import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { userStored } from "../../recoil/user/atoms";
-
-
 
 function Button({ onClick = () => {}, loading = false, children }) {
   return (
@@ -73,7 +71,10 @@ function Password({ register, error, name, label }) {
 function EmailInput({ register, error }) {
   return (
     <div className="col-span-6 sm:col-span-3">
-      <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium text-gray-700"
+      >
         Email Address
       </label>
       <input
@@ -94,8 +95,6 @@ function EmailInput({ register, error }) {
   );
 }
 
-  
-
 function LoginForm() {
   const {
     register,
@@ -105,33 +104,31 @@ function LoginForm() {
 
   const navigate = useNavigate();
 
-  const [user,setUser] = useRecoilState(userStored);
+  const [user, setUser] = useRecoilState(userStored);
 
   const onSubmit = async (data) => {
-    await http.post('auth/login', {
-      email: data.email,
-      password: data.password
-    })
-    .then((res) => {
-      console.log(res.data)
-      // const token = res.data
-      // useEffect(() => {
-      //   setUser([
-      //     {
-      //       token
-      //     }
-      //   ])
+    await http
+      .post("auth/login", {
+        email: data.email,
+        password: data.password,
+      })
+      .then((res) => {
+        console.log(res.data);
+        // const token = res.data
+        // useEffect(() => {
+        //   setUser([
+        //     {
+        //       token
+        //     }
+        //   ])
 
-      // }, [])
-      
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-
-    
+        // }, [])
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
-  
+
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8 bg-gray-50 p-5">
@@ -142,7 +139,10 @@ function LoginForm() {
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{" "}
-            <Link to="/signup" className="font-medium text-indigo-600 hover:text-indigo-500">
+            <Link
+              to="/signup"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
               Sign up
             </Link>
           </p>
@@ -150,11 +150,11 @@ function LoginForm() {
         <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
           <EmailInput register={register} error={errors?.email} />
           <Password
-                register={register}
-                error={errors?.password}
-                name="password"
-                label="Password"
-              />
+            register={register}
+            error={errors?.password}
+            name="password"
+            label="Password"
+          />
           <div className="flex items-center justify-between">
             <div className="flex items-center">
               <input
@@ -163,39 +163,46 @@ function LoginForm() {
                 type="checkbox"
                 className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
               />
-              <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+              <label
+                htmlFor="remember-me"
+                className="ml-2 block text-sm text-gray-900"
+              >
                 Remember me
               </label>
             </div>
 
             <div className="text-sm">
-              <Link to="/userfind" className="font-medium text-indigo-600 hover:text-indigo-500">
+              <Link
+                to="/userfind"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
                 Forgot your password?
               </Link>
             </div>
           </div>
 
           <div>
-              <Button onClick={handleSubmit(onSubmit)} loading={isSubmitting}>
-                Sign In
-              </Button>
+            <Button onClick={handleSubmit(onSubmit)} loading={isSubmitting}>
+              Sign In
+            </Button>
           </div>
           <div>
-            <Link to="/auth/github">
-              <button className="group relative flex w-full justify-center rounded-md border py-2 px-4 text-sm font-medium hover:bg-gray-100">
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z" />
-                  </svg>
-                </span>
-                Sign in with Github
-              </button>
-            </Link>
+            <a
+              href={`${import.meta.env.VITE_API_URL}oauth/redirect/github`}
+              className="group relative flex w-full justify-center rounded-md border py-2 px-4 text-sm font-medium hover:bg-gray-100"
+            >
+              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z" />
+                </svg>
+              </span>
+              Sign in with Github
+            </a>
           </div>
         </form>
       </div>
