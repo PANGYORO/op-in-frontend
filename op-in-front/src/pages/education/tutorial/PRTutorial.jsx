be4729fe87811d0c3b197922a7fc42b0b36975f4
Sungwook Cho <saeloun8797@gmail.com>
2023-02-08 09:18:40 +0900

    S08P12C211-494 tutorial 파일 작성

---
+++ op-in-front/src/pages/education/tutorial/PRTutorial.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+function PRTutorial() {
+  return(
+    <embed data="https://github.com/"
+    width="800"
+    height="800"
+    type="text/html">
+    </embed>
+  );
+}
+
+export default PRTutorial
