fa174cc12d39d4f14db047e1180af180b37db39f
kcy0521 <kyle.kcy@gmail.com>
2023-02-08 16:11:15 +0900

    S08P12C211-495 repo merge

--- op-in-front/src/main.jsx
+++ op-in-front/src/main.jsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from "react";
+// import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
@@ -8,13 +8,13 @@ import { RecoilRoot } from "recoil";
 import Main from "./pages/Main";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <BrowserRouter>
-        <StrictMode>
-          <Main />
-        </StrictMode>
-      </BrowserRouter>
-    </RecoilRoot>
-  </React.StrictMode>
+  // <React.StrictMode>
+  // {/* </React.StrictMode> */}
+  <RecoilRoot>
+    <BrowserRouter>
+      {/* <StrictMode>
+      </StrictMode> */}
+      <Main />
+    </BrowserRouter>
+  </RecoilRoot>
 );
