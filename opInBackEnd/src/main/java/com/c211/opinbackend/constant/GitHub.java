5859557bb68664697d52d0d398b5accbe396a42f
djunnni <djunnni@gmail.com>
2023-02-08 12:02:47 +0900

    test(Github): Constant 변수 생성 테스트

--- opInBackEnd/src/main/java/com/c211/opinbackend/constant/GitHub.java
+++ opInBackEnd/src/main/java/com/c211/opinbackend/constant/GitHub.java
@@ -5,4 +5,14 @@ public class GitHub {
 	public static final String AUTHORIZE_URL = "https://github.com/login/oauth/authorize";
 	public static final String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
 	public static final String USER_INFO_URL = "https://api.github.com/user";
+	public static final String PUBLIC_USER_INFO_URL = "https://api.github.com/users/{USERNAME}";
+	public static final String PUBLIC_USER_REPO_URL = "https://api.github.com/users/{USERNAME}/repos";
+
+	public static String getUserInfoUrl(String githubUserName) {
+		return PUBLIC_USER_INFO_URL.replace("{USERNAME}", githubUserName);
+	}
+
+	public static String getUserRepoUrl(String githubUserName) {
+		return PUBLIC_USER_REPO_URL.replace("{USERNAME}", githubUserName);
+	}
 }
--- opInBackEnd/src/test/java/com/c211/opinbackend/constant/GithubTest.java
+++ opInBackEnd/src/test/java/com/c211/opinbackend/constant/GithubTest.java
@@ -4,9 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-public class Github {
+public class GithubTest {
 	@Test
 	public void USER_REPOSITORY_URL_TEST() {
 		assertEquals("https://api.github.com/users/Djunnni/repos", GitHub.getUserRepoUrl("Djunnni"));
+		assertEquals("https://api.github.com/users/MJ.MJ/repos", GitHub.getUserRepoUrl("MJ.MJ"));
 	}
 }
