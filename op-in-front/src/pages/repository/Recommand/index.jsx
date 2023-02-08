501b7ef9c8b3cdd10a3cee6c0a7a4fbcef1b971b
kcy0521 <kyle.kcy@gmail.com>
2023-02-08 09:29:39 +0900

    S08P12C211-495 merge

--- op-in-front/src/pages/repository/Recommand/index.jsx
+++ op-in-front/src/pages/repository/Recommand/index.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RecommandRepo from "@components/repository/RecommandRepo";
+import http from "@api/http";
+import { useLocation } from "react-router-dom";
+
 
 //axios 사용해서 정보 전달
+
 
 // const arr = ['배열 요소1', '배열 요소2', '배열 요소3'];
 // arr.map((elem, index) => {
@@ -15,6 +19,30 @@ import RecommandRepo from "@components/repository/RecommandRepo";
 // })
 
 function RecommandIndex() {
+
+  const [myinfo, setMyInfo] = useState("");
+  const location = useLocation();
+  const currentEmail = location.state;
+
+  async function RepoData() {
+    await http
+      .post(`repo/member`, {
+        email: currentEmail,
+      })
+      .then((response) => {
+        // alert(currentEmail);
+        alert(response.data.nickname);
+        console.log(response.data);
+        setMyInfo(response.data);
+      })
+      .catch(() => alert("error"));
+    console.log(myinfo);
+  }
+
+  useEffect(() => {
+    RepoData();
+  }, []);
+
   return (
     <div className="grid grid-cols-1 gap-4 mt-4 ml-4">
       <RecommandRepo />
