71065d5b3951e016d71cd1b7b5f18d025d079c9b
swany0509 <swany0509@naver.com>
2023-02-08 10:45:05 +0900

    hotfix-sw

--- op-in-front/src/api/http.js
+++ op-in-front/src/api/http.js
@@ -3,8 +3,8 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 // axios 객체 생성
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  // baseURL: "http://i8c211.p.ssafy.io:5001/",
+  // baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://i8c211.p.ssafy.io:5001/",
   // baseURL: "http://127.0.0.1:5001/",
   headers: {
     "Content-Type": "application/json;charset=utf-8",
