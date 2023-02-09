f5709fd45247b95ac43d93ec18ce75c52e7bd267
djunnni <djunnni@gmail.com>
2023-02-09 14:32:54 +0900

    feat(batch): 레포지터리 이력 저장하기

---
+++ opInBackEnd/src/main/java/com/c211/opinbackend/batch/dto/github/ContributorDto.java
@@ -0,0 +1,33 @@
+package com.c211.opinbackend.batch.dto.github;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ContributorDto {
+	private String login;
+	private Long id;
+	@JsonProperty("avatar_url")
+	private String avatarUrl;
+
+	private String type;
+
+	@JsonProperty("url")
+	private String userInfoUrl;
+
+	@JsonProperty("followers_url")
+	private String followersUrl;
+
+	@JsonProperty("following_url")
+	private String followingUrl;
+
+	@JsonProperty("repos_url")
+	private String reposUrl;
+
+	public Long contributions;
+}
