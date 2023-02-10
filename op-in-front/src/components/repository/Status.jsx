865e0e0b1dbb891b782f931800bcdf47be94cca5
djunnni <djunnni@gmail.com>
2023-02-10 11:57:04 +0900

    fix(repository): Status, RepoDetail 수정

--- op-in-front/src/components/repository/Status.jsx
+++ op-in-front/src/components/repository/Status.jsx
@@ -15,14 +15,22 @@ export default function Status({ repoDetail }) {
       for (let i = 0; i < (list.length < 5 ? list.length : 5); i++) {
         result.push(
           <>
-            <Link id={"cont" + list[i].id} to={`/userdetail`} state={list[i].nickname} key={i}>
+            <Link
+              id={"cont" + list[i].id}
+              to={`/userdetail`}
+              state={list[i].nickname}
+              key={i}
+            >
               <img
                 className="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
                 src={DefaultImg}
                 alt={list[i].nickname}
               />
             </Link>
-            <Tooltip anchorId={"cont" + list[i].id} content={list[i].nickname} />
+            <Tooltip
+              anchorId={"cont" + list[i].id}
+              content={list[i].nickname}
+            />
           </>
         );
       }
@@ -38,15 +46,18 @@ export default function Status({ repoDetail }) {
             type="button"
             className="fpx-3 py-1 mb-3 text-base text-blue-600 bg-blue-200 rounded-full mx-1"
           >
-            {list[i]}
+            {list[i]?.title}
           </button>
         );
       }
     return result;
   };
   const moreContributors = () => {
-    if (repoDetail.contributors != null && repoDetail.contributors.length > 4) {
-      return repoDetail.contributors.length - 4 + " more Contributors...";
+    if (
+      repoDetail?.contributors != null &&
+      repoDetail?.contributors.length > 4
+    ) {
+      return repoDetail?.contributors.length - 4 + " more Contributors...";
     } else return "";
   };
   return (
@@ -57,21 +68,28 @@ export default function Status({ repoDetail }) {
           <div className="block w-full h-full">
             <div className="w-full">
               <div className="flex items-center">
-                <div className="flex -space-x-2">{contributerRender(repoDetail.contributors)}</div>
+                <div className="flex -space-x-2">
+                  {contributerRender(repoDetail?.contributors)}
+                </div>
               </div>
-              <div className="mt-2 text-blue-500 dark:text-gray-300" onClick={toggleModal}>
+              <div
+                className="mt-2 text-blue-500 dark:text-gray-300"
+                onClick={toggleModal}
+              >
                 {moreContributors()}
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="my-2">{repoDetail.content}</div>
+      <div className="my-2">{repoDetail?.content}</div>
       <div className="my-2 text-gray-400 dark:text-white-300">
-        {repoDetail.updateDate == null ? "no data" : repoDetail.updateDate}
+        {repoDetail?.updateDate == null ? "no data" : repoDetail?.updateDate}
       </div>
       <div className="my-2 font-bold">About</div>
-      <div className="grid grid-cols-3 my-2">{tagRender(repoDetail.techLangs)}</div>
+      <div className="grid grid-cols-3 my-2">
+        {tagRender(repoDetail?.techLangs)}
+      </div>
       <div className="m-4">
         {/* <div>
           <div className="inline-flex">
@@ -108,7 +126,7 @@ export default function Status({ repoDetail }) {
               d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
             />
           </svg>
-          stars : {repoDetail.star}
+          stars : {repoDetail?.star}
         </div>
         {/* 
         <div>
@@ -151,11 +169,15 @@ export default function Status({ repoDetail }) {
                 d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
               />
             </svg>
-            forks : {repoDetail.forkNum}
+            forks : {repoDetail?.forkNum}
           </div>
         </div>
       </div>
-      <ContributorsModal open={open} setOpen={setOpen} contributors={repoDetail.contributors} />
+      <ContributorsModal
+        open={open}
+        setOpen={setOpen}
+        contributors={repoDetail?.contributors}
+      />
     </div>
   );
 }
