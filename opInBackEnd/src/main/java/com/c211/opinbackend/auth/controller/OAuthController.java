d83c54dc12e464c0f2e426ff0d30ff7589a63b34
djunnni <djunnni@gmail.com>
2023-02-10 16:09:51 +0900

    fix(OAuth): 깃헙 로그인시, maxAge 설정

--- opInBackEnd/src/main/java/com/c211/opinbackend/auth/controller/OAuthController.java
+++ opInBackEnd/src/main/java/com/c211/opinbackend/auth/controller/OAuthController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,11 @@ public class OAuthController {
 
 	private static final Logger logger = LoggerFactory.getLogger(OAuthController.class);
 	OAuthService oAuthService;
+	@Value("${jwt.access-token-validity-in-seconds}")
+	private long accessTokenValidityInSeconds;
+
+	@Value("${jwt.refresh-token-validity-in-seconds}")
+	private long refreshTokenValidityInSeconds;
 
 	@Autowired
 	public OAuthController(OAuthServiceImpl oAuthService) {
@@ -53,8 +59,11 @@ public class OAuthController {
 			Cookie typeCookie = new Cookie("type", token.getType());
 
 			accessTokenCookie.setPath("/");
+			accessTokenCookie.setMaxAge(((int)accessTokenValidityInSeconds / 1000) - 1);
 			refreshTokenCookie.setPath("/");
+			refreshTokenCookie.setMaxAge(((int)refreshTokenValidityInSeconds / 1000) - 1);
 			typeCookie.setPath("/");
+			typeCookie.setMaxAge(((int)accessTokenValidityInSeconds / 1000) - 1);
 
 			response.addCookie(accessTokenCookie);
 			response.addCookie(refreshTokenCookie);
