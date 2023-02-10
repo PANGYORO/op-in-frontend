37436cd5fc406162a7f7865084cf44114cff69c3
chosunguk <saeloun8797@gmail.com>
2023-02-10 13:28:33 +0900

    S08P12C211-494 tutorial 구성

--- op-in-front/src/pages/education/tutorial/PRTutorial.jsx
+++ op-in-front/src/pages/education/tutorial/PRTutorial.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player'
 import fork1 from '@assets/video/fork1.mov'
 import fork2 from '@assets/video/fork2.mov'
@@ -6,18 +6,37 @@ import clone from '@assets/video/clone.mov'
 
 
 
+
 function PRTutorial() {
+
+  const contentsList = [fork1, fork2, clone]
+  const [curNum, setCurNum] = useState(0)
+  const [curContent, setCurContent] = useState(contentsList[curNum])
+
+  const nextBtn = () => {
+    setCurNum( curNum => curNum + 1)
+    console.log(curNum)
+    setCurContent(contentsList[curNum])
+    
+  }
+  
   return (
     <div className='player-wrapper'>
       <ReactPlayer 
-        url={fork1} 
+        url={curContent} 
         loop={true} 
         playing={true}
         muted={true} 
         height='500px'
         width='800px'
       />
+      <button
+        onClick={nextBtn}
+      >next
+      </button>
     </div>
+
+    
   );
 }
 
