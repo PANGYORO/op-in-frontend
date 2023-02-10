6634ca575913ceafc2a3ccf5691d970860062415
Sungwook Cho <saeloun8797@gmail.com>
2023-02-10 09:09:09 +0900

    S08P12C211-494 reactplayer 영상 구현

--- op-in-front/src/pages/education/tutorial/PRTutorial.jsx
+++ op-in-front/src/pages/education/tutorial/PRTutorial.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
+import fork1 from '@assets/video/fork1.mov'
+import fork2 from '@assets/video/fork2.mov'
+import clone from '@assets/video/clone.mov'
 
 
 
 function PRTutorial() {
   return (
-    <div>
-
+    <div className='player-wrapper'>
+      <ReactPlayer 
+        url={fork1} 
+        loop={true} 
+        playing={true}
+        muted={true} 
+        height='500px'
+        width='800px'
+      />
     </div>
   );
 }
