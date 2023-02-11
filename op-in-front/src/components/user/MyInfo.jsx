7a98a7ac8f577311ef4528d7dbc6845e74549721
swany0509 <swany0509@naver.com>
2023-02-11 19:05:43 +0900

    S08P12C211-494 merge

--- op-in-front/src/components/user/MyInfo.jsx
+++ op-in-front/src/components/user/MyInfo.jsx
@@ -8,13 +8,29 @@ import DeleteModal from "@components/modals/DeleteModal";
 import { userInfo } from "@recoil/user/atoms";
 import { useRecoilValue } from "recoil";
 
+const renderRepos = (list = []) => {
+  return list.map((item, index) => (
+    <li
+      key={item.id}
+      className="flex items-center justify-between py-3 text-gray-600 border-b-2 border-gray-100 dark:text-gray-200 dark:border-gray-800"
+    >
+      <div
+        className="flex items-center justify-start text-sm"
+        // onClick={() =>
+        //   visitRepo(item.id,item,title);
+        // }
+      >
+        <span className="mx-4">{index + 1}</span>
+        <span>{item.title}</span>
+      </div>
+    </li>
+  ));
+};
+
 // Language , Topic
-export default function MyInfo({ currentuser }) {
+const MyInfo = ({ currentuser }) => {
   const [open, setOpen] = useState(false);
   const user = useRecoilValue(userInfo);
-
-  // console.log(currentuser);
-  // console.log(currentuser.techLanguages);
 
   function toggleModal() {
     setOpen((prev) => !prev);
@@ -68,32 +84,7 @@ export default function MyInfo({ currentuser }) {
             ({currentuser.contributeRepo == null ? 0 : currentuser.contributeRepo.length})
           </span>
         </p>
-        <ul>
-          <li className="flex items-center justify-between py-3 text-gray-600 border-b-2 border-gray-100 dark:text-gray-200 dark:border-gray-800">
-            <div className="flex items-center justify-start text-sm">
-              <span className="mx-4">01</span>
-              <span>Facebook</span>
-            </div>
-          </li>
-          <li className="flex items-center justify-between py-3 text-gray-600 border-b-2 border-gray-100 dark:text-gray-200 dark:border-gray-800">
-            <div className="flex items-center justify-start text-sm">
-              <span className="mx-4">02</span>
-              <span>Git</span>
-            </div>
-          </li>
-          <li className="flex items-center justify-between py-3 text-gray-600 border-b-2 border-gray-100 dark:text-gray-200 dark:border-gray-800">
-            <div className="flex items-center justify-start text-sm">
-              <span className="mx-4">03</span>
-              <span>naver</span>
-            </div>
-          </li>
-          <li className="flex items-center justify-between py-3 text-gray-600 border-b-2 border-gray-100 dark:text-gray-200 dark:border-gray-800">
-            <div className="flex items-center justify-start text-sm">
-              <span className="mx-4">04</span>
-              <span>google</span>
-            </div>
-          </li>
-        </ul>
+        <ul>{renderRepos(currentuser.contributeRepo)}</ul>
         <hr />
         <p className="p-4 font-bold text-black text-md dark:text-white">
           following Respsitories
@@ -101,31 +92,7 @@ export default function MyInfo({ currentuser }) {
             ({currentuser.followRepo == null ? 0 : currentuser.followRepo.length})
           </span>
         </p>
-        <ul>
-          <li className="flex items-center justify-between py-3 text-gray-600 border-b-2 border-gray-100 dark:text-gray-200 dark:border-gray-800">
-            <div className="flex items-center justify-start text-sm">
-              <span className="mx-4">01</span>
-              <span>Facebook</span>
-            </div>
-          </li>
-          <li className="flex items-center justify-between py-3 text-gray-600 border-b-2 border-gray-100 dark:text-gray-200 dark:border-gray-800">
-            <div className="flex items-center justify-start text-sm">
-              <span className="mx-4">02</span>
-              <span>Git</span>
-            </div>
-          </li>
-          <li className="flex items-center justify-between py-3 text-gray-600 border-b-2 border-gray-100 dark:text-gray-200 dark:border-gray-800">
-            <div className="flex items-center justify-start text-sm">
-              <span className="mx-4">03</span>4 <span>naver</span>
-            </div>
-          </li>
-          <li className="flex items-center justify-between py-3 text-gray-600 border-b-2 border-gray-100 dark:text-gray-200 dark:border-gray-800">
-            <div className="flex items-center justify-start text-sm">
-              <span className="mx-4">04</span>
-              <span>google</span>
-            </div>
-          </li>
-        </ul>
+        <ul>{renderRepos(currentuser.followRepo)}</ul>
         {currentuser.nickname == user.nickname ? (
           <div className="grid grid-cols-1 justify-items-center py-4">
             <button
@@ -149,4 +116,5 @@ export default function MyInfo({ currentuser }) {
       <DeleteModal open={open} setOpen={setOpen} />
     </div>
   );
-}
+};
+export default MyInfo;
