1b465881737341e3d97bfc627736858e663bbabd
kcy0521 <kyle.kcy@gmail.com>
2023-02-12 20:38:47 +0900

    S08P12C211-495 qna 수정,삭제 색연하게

--- op-in-front/src/components/repository/QnA.jsx
+++ op-in-front/src/components/repository/QnA.jsx
@@ -110,14 +110,14 @@ const QnA = ({
                   <button
                     type="button"
                     onClick={modifyQnA}
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    className="text-white bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                   >
                     Modify
                   </button>
                   <button
                     type="button"
                     onClick={removeQnA}
-                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-1 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                    className="focus:outline-none text-white bg-red-500 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-1 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                   >
                     Delete
                   </button>
