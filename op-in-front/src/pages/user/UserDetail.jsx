10f15cc7aaae250b25a43be1a2be893b857a3f82
Dongjoon Lee <djunnni@gmail.com>
2023-02-14 16:18:26 +0900

    fix(MyInfo): currentUser camelCase 수정

--- op-in-front/src/pages/user/UserDetail.jsx
+++ op-in-front/src/pages/user/UserDetail.jsx
@@ -232,10 +232,7 @@ const UserDetail = () => {
               <div>
                 {/* 오른쪽 상단 */}
                 <div className="grid grid-cols-2 gap-2 justify-items-between">
-                  <div className="self-center">
-                    {" "}
-                    {myInfo.nickname}
-                  </div>
+                  <div className="self-center"> {myInfo.nickname}</div>
 
                   <div className="self-center">
                     {isMe ? (
@@ -286,7 +283,7 @@ const UserDetail = () => {
           <div className="pt-2 pb-24 pl-2 pr-2  md:pt-0 md:pr-0 md:pl-0">
             <div className="flex flex-col flex-wrap sm:flex-row h-full">
               <div className="w-1/3 h-full ">
-                <MyInfo currentuser={myInfo} />
+                <MyInfo currentUser={myInfo} />
               </div>
 
               <div className=" w-2/3  ">
